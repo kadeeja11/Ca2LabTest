aaafff132b86b875c3f1a2ca844c3bdcc441d753
unknown <SIL@AMCS-SIL-51.cs2k16.amcs.ws2k12.psgcs.edu>
2024-03-23 15:59:11 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+const axios = require('axios');
 
-let fetch;
-
-// Dynamically import 'node-fetch' using import()
-(async () => {
-    const fetchModule = await import('node-fetch');
-    fetch = fetchModule.default;
-})();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Enable CORS middleware
-app.use(cors());
-
-app.get('/random-meal', async (req, res) => {
-    try {
-        if (!fetch) {
-            throw new Error('Fetch module not imported yet');
-        }
-
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
-        if (!response.ok) {
-            throw new Error(`Request failed with status ${response.status}`);
-        }
-
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+const fetchRecipe = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://low-carb-recipes.p.rapidapi.com/recipes/2807982c-986a-4def-9e3a-153a3066af7a',
+    headers: {
+      'X-RapidAPI-Key': '3d22db8984msh64a48e552b6ce93p11d34ajsndc212bc8bccc',
+      'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
     }
-});
+  };
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchRecipe();
